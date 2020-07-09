124e5ec489cb6e636ad85aa10c64e82ae8a3e8f9
TarLeeProject <68064288+TarLeeProject@users.noreply.github.com>
2020-07-09 20:43:23 +0700

    Update B1.cpp

--- B1.cpp
+++ B1.cpp
@@ -5,12 +5,6 @@ using namespace std;
 
 int main(){
     
-    float  t=0;
-    for(int i=0; i<=1000; i++){
-        t=t+((float)pow(-1,i)/(2*i+1));
-    }
-    
-    cout<<(float)t;
     
     return 0;
 }
